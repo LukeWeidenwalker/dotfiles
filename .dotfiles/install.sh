2c240cf8b82f403eb0152b3f18a7ad0d3bfe490c
Lukas Weidenholzer <lukas.weidenholzer@gmail.com>
2022-05-20 22:08:32 +0200

    add vscode keybindings and snippets

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,3 +1,7 @@
 # Symlink VSCode settings (target location might change, see here: https://code.visualstudio.com/docs/getstarted/settings#_settings-file-locations)
 rm ~/.config/Code/User/settings.json
 ln -s ~/.vscode/settings.json ~/.config/Code/User/settings.json
+rm ~/.config/Code/User/keybindings.json ~/.vscode/keybindings.json
+ln -s ~/.vscode/keybindings.json ~/.config/Code/User/keybindings.json
+rm ~/.config/Code/User/snippets ~/.vscode/
+ln -s ~/.vscode/snippets ~/.config/Code/User/
